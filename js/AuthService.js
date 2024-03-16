cbdca70d9a0e3fea0a4fb1fd938b29242f819e4a
Albara Hassan <braa3300@hotmail.com>
2024-03-16 23:47:53 +0100

    feat(M1): added validation for forms

--- js/AuthService.js
+++ js/AuthService.js
@@ -14,10 +14,21 @@ $(document).ready(() => {
 
     $('.login-form').submit((event) => {
         event.preventDefault();
+
+        // Get email and password from the form
+        let email = $('#email').val().trim();
+        let password = $('#password').val().trim();
+
+        if (email === '' || password === '') {
+            $('#error-message').text('Email and password are required').show();
+            return;
+        }
+
         let formData = {
-            email: $('#email').val(),
-            password: $('#password').val()
+            email: email,
+            password: password
         };
+
         $.ajax({
             type: 'POST',
             url: '/api/auth/login',
@@ -38,18 +49,42 @@ $(document).ready(() => {
 
     $('.signup-form').submit((event) => {
         event.preventDefault();
+
+        let email = $('#email').val().trim();
+        let password = $('#password').val().trim();
+        let username = $('#username').val().trim();
+        let passwordConfirmation = $('#repeat').val().trim();
+
+        if (email === '' || password === '' || username === '' || passwordConfirmation === '') {
+            $('#error-message').text('Email, password, and username are required').show();
+            return;
+        }
+
+        else if(passwordConfirmation !== password){
+            $('#error-message').text('Password Confirmation should match').show();
+            return;
+        }
+
+        if (password.length < 6) {
+            $('#error-message').text('Password must be at least 6 characters long').show();
+            return;
+        }
+
         let formData = {
-            email: $('#email').val(),
-            password: $('#password').val(),
-            username: $('#username').val(),
+            email: email,
+            password: password,
+            username: username
         };
+
         $.ajax({
             type: 'POST',
             url: '/api/auth/signup',
             data: JSON.stringify(formData),
             contentType: 'application/json',
             success: (response) => {
+                // Store token in localStorage
                 localStorage.setItem('token', response.token);
+                // Redirect to home page
                 location.replace('#home');
             },
             error: (xhr, status, error) => {
